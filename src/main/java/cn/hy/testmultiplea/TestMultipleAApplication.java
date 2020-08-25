64a0dfc459df59b278234d285657efd7e3adce25
zhaoxiangxiong <zhaoxiangxiong@haoyuntech.com>
2020-08-25 10:25:09 +0800

    a

---
+++ src/main/java/cn/hy/testmultiplea/TestMultipleAApplication.java
@@ -0,0 +1,13 @@
+package cn.hy.testmultiplea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMultipleAApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestMultipleAApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/hy/testmultiplea/TestMultipleAApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.hy.testmultiplea;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMultipleAApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
